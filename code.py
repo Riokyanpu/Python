da0bf666ac72d2c5ba2dbd66c9c72f50e35fc2b3
Riokyanpu <kgst1216@gmail.com>
2024-05-20 18:12:44 +0900

    HTMLを表示

--- code.py
+++ code.py
@@ -1,3 +1,8 @@
 # coding: utf-8
 # hello worldと表示する
-print("holle world")
+#プログラミングでは、「print」のような関数で、コンピュータに対して動作を指示していきます。
+# 「print」と書くと、文字を表示するのだなとPythonが理解
+print("holle world") #文字を表示
+'''
+print("holle world") #この間は表示されない（シングルクォーツ）
+'''
---
+++ code2.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+#HTMLを表示する
+print("<h1>hello world</h1>")
+print("<p>世界の皆さん")
+print("<b>こんにちは</b></p>")
+
+#シングルクォーツの場合
+print('''<h1>hello world</h1>
+<p>世界の皆さん
+<b>こんにちは</b></p>''')
+
+#print関数で改行しない場合
+print("<h1>hello world</h1>","<p>世界の皆さん</p>","<b>こんにちは</b></p>")
+
+#print関数の括弧中でダブルクォーター後にカンマとendとダブルクォーター✖️２
+print("<h1>hello world</h1>",end="")
+print("<p>世界の皆さん",end="")
+print("<b>こんにちは</b></p>",end="")
