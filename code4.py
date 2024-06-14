2a067e02defed286a05eb6b6334c0319e62bfef6
Riokyanpu <kgst1216@gmail.com>
2024-06-14 12:25:04 +0900

    if 2
    import random追加

--- code4.py
+++ code4.py
@@ -5,3 +5,11 @@ if number ==1:
     print("OK!")  #条件式が成立したときの処理
 else:
     print("NO")  #条件式が成立しなかったときの処理
+    
+# coding: utf-8
+# if文による条件分岐
+import random
+number = random.randint(1,3) * 100
+print("あなたの得点は" + str(number) + "ポイントです")
+if number == 300:
+    print("おめでとう")
