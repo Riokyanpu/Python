892c07d7e779e9a5aab1bc18e7a2c4a6ea610225
Riokyanpu <kgst1216@gmail.com>
2024-08-30 11:37:38 +0900

    計算1

--- code3.py
+++ code3.py
@@ -50,6 +50,14 @@ print("ぶどうを買う数:" + str(grapes_num) + "個")
 total = grapes_price * grapes_num
 print("合計金額" + str(total) + "円")
 
+#体重の計算
+import random
+number = random.randint(1, 10)	# 匹数 1 ～ 10
+print("体重100キロのスライムが" + str(number) + "匹あらわれた")
+# 合計体重 = 匹数 x 100
+total = number * 100
+print("スライムの合計体重は" + str(total) + "キロです")
+
 # 匹数を表示する
 print("体重100キロのスライムが" + str(number) + "匹あらわれた")
 
