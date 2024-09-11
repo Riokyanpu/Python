ec4dd84fd7d6cdca9892305aaa3f3967d5198866
Riokyanpu <kgst1216@gmail.com>
2024-09-11 09:52:17 +0900

    簡単なIF分

--- code4.py
+++ code4.py
@@ -1,10 +1,10 @@
 # coding: utf-8
 # if文による条件分岐
 number = 2
-if number ==1:
-    print("OK!")  #条件式が成立したときの処理
+if number == 1:
+    print( "成功")  #条件式が成立したときの処理
 else:
-    print("NO")  #条件式が成立しなかったときの処理
+    print( "失敗")  #条件式が成立しなかったときの処理
     
 # coding: utf-8
 # if文による条件分岐
