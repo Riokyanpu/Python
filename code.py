1f14b6acfab1bc7d1f6ce681346d2c28c24cada1
Riokyanpu <kgst1216@gmail.com>
2024-10-29 14:26:53 +0900

    ポートフォリオ作成1-1

--- code.py
+++ code.py
@@ -15,3 +15,40 @@ print("hello world3")
 #複数の行をコメントアウトするときは
 #3つの連続したシングルクォートで囲む
 
+ 
+#＃04:HTMLを表示してみよう
+# coding: utf-8
+#HTMLを表示する
+print("<h1>hello world</h1>")
+print("<p>世界の皆さん、")
+print("<b>こんにちは</b></P>")
+
+print('''<h1>hello world</h1>
+<p>世界の皆さん、
+<b>こんにちは</b></p>''') #シングルクォーテーション３個で、複数の行を扱うことができる。
+
+print("<h1>hello world</h>","<p>世界の皆さん、","<b>こんにちは</b><p>") #改行したくない場合、一つ『,』で区切る
+
+#演習問題
+print("勇者は、荒野を歩いていた")
+print("<b>モンスター</b>があらわれた")
+
+#05:変数を使えるようになろう
+# 変数を使う
+player = "戦士"
+print(player +"は、荒野を歩いていた") #文字データを格納した変数と文字列は、「+」記号で連結
+print(player +"は、モンスターと戦った")
+print(player + "は、モンスターをたおした")
+
+#06:サイコロを作ろう
+#数の表示とサイコロ
+
+import random
+number = random.randint(10,50)
+print("スライムが" + str(number) + "匹あらわれた") #str(number) ：数値を、数を表す文字に変換する
+
+'''
+importを挿入することで『random,randin』関数を使用することができる
+random.random() ：0から1までのランダムな数値を出力する
+random.randint(a, b) ：aからbまでの間のランダムな整数を出力する
+'''
